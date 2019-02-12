2eec1d3d66c78f25204f1adde8427f63ac20b9a9
Benjamin Bidmead <pry@Ghostbook-Air.home>
2019-02-12 20:28:40 +0000

    added easteregg

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,9 +21,20 @@ $(document).ready(function() {
   					`</button>` +
 					`</div>`
 
+		let message2 = `<div id="message-badass" class="alert alert-primary alert-dismissible fade show" role="alert">` +
+  					`<strong>Wow!</strong> You really are a badass. You're invited to 0x00sec.org` +
+  					`<button type="button" class="close" data-dismiss="alert" aria-label="Close">` + 
+    					`<span aria-hidden="true">&times;</span>` +
+  					`</button>` +
+					`</div>`
+
 		$("#broken-counter").html(counter);
-		if (counter == 7) {
+		if (counter == 5) {
 			$("#video").append(`<iframe width="560" height="315" src="https://www.youtube.com/embed/L397TWLwrUU?autoplay=1" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
+		}
+
+		if (counter == 34) {
+			$("#messages").append(message2)
 		}
 		$("#messages").append(message);
     	$("#message-" + counter).fadeTo(2000, 0).slideUp(500, function(){
